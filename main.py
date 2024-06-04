19b71b07b4b8570c93fcb9444556b7ec94e009d0
Jacob Carter <jacobcarter@llexeter.co.uk>
2024-06-04 13:56:13 +0100

    auth correction

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 carterapi_baseurl = "http://127.0.0.1:5000/"
 
+HEADERS = {
+        'x-api-key': "i0l765RJ30f9HR47L072c2tc74V1597h",
+        'Content-Type': 'application/json'
+    }
+
 
 @app.route("/")
 def index():
@@ -19,50 +24,52 @@ def index():
     request_data = {
     }
 
-    headers = {
-        'x-api-key': "abc",
-        'Content-Type': 'application/json'
-    }
-
-    response = requests.post(f"{carterapi_baseurl}/get/portfolio-config", headers=headers, json=request_data)
-    data = response.json()["data"]
-
-    data["colours"] = data["colours"][mode]
-
-    favicon_data = base64.b64decode(data["favicon"])
-
-    with open("static/assets/icons/favicon.ico", 'wb') as ico_file:
-        ico_file.write(favicon_data)
-
-    segments = {}
-
-    for segment_id in data["segments"]:
-        request_data = {
-            "id": segment_id
-        }
-        response = requests.post(f"{carterapi_baseurl}/get/portfolio-segment", headers=headers, json=request_data)
-        segment_data = response.json()["data"]
-        segment_name = segment_data["name"]
-        segment_data.pop("name")
-        segments.update({segment_name: segment_data})
-
-    show_icons = True
-    show_banners = True
-
-    if mode == "dark":
-        notmode = "light"
+    try:
+        response = requests.post(f"{carterapi_baseurl}/get/portfolio-config", headers=HEADERS, json=request_data)
+    except requests.exceptions.ConnectionError as e:
+        return "API inaccessible. Please try again later.<br><br>" + str(e)
     else:
-        notmode = "dark"
+        try:
+            data = response.json()["data"]
 
-    return render_template(
-        "index.html",
-        data=data,
-        segments=segments,
-        show_icons=show_icons,
-        show_banners=show_banners,
-        mode=mode,
-        notmode=notmode,
-    )
+            data["colours"] = data["colours"][mode]
+
+            favicon_data = base64.b64decode(data["favicon"])
+
+            with open("static/assets/icons/favicon.ico", 'wb') as ico_file:
+                ico_file.write(favicon_data)
+
+            segments = {}
+
+            for segment_id in data["segments"]:
+                request_data = {
+                    "id": segment_id
+                }
+                response = requests.post(f"{carterapi_baseurl}/get/portfolio-segment", headers=HEADERS, json=request_data)
+                segment_data = response.json()["data"]
+                segment_name = segment_data["name"]
+                segment_data.pop("name")
+                segments.update({segment_name: segment_data})
+        except KeyError as e:
+            return "API response error. Please try again later.<br><br>" + str(response.json())
+
+        show_icons = True
+        show_banners = True
+
+        if mode == "dark":
+            notmode = "light"
+        else:
+            notmode = "dark"
+
+        return render_template(
+            "index.html",
+            data=data,
+            segments=segments,
+            show_icons=show_icons,
+            show_banners=show_banners,
+            mode=mode,
+            notmode=notmode,
+        )
 
 
 @app.route('/favicon.ico')
